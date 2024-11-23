748d34d7bb8c8b1bd43f1ec47c88c4d979460d4d
ItzC4k3y <thanaphat.athi@gmail.com>
2024-11-24 01:37:59 +0700

    PauseMenu + Main Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+       
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused) 
+            {
+                Resume();
+            }
+
+            else
+            {
+               Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu");
+    }
+}
