4145fe35cd9464d090d952b11492bc550cd60566
ItzC4k3y <thanaphat.athi@gmail.com>
2024-12-07 01:10:53 +0700

    added healpotion

---
+++ Assets/Scripts/HealPotion.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPotion : PickUps
+{
+    [SerializeField] private float heal;
+
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        base.OnTriggerEnter2D(collision);
+
+        if (player.Health >= 60)
+            { 
+            player.Health = 60; 
+            }
+
+        else
+        {
+            player.Health += heal;
+            player.PlayerHealthBar.UpdateHp(player.Health);
+            
+        }
+    }
+  
+}
